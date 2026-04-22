8720921f6ac1cca3e2d10bdb4846e59dc9e247bf
Daniel Espinoza <daniel.espinoza.g@gmail.com>
2026-04-22 10:39:01 -0500

    Actualización del código: corrección de errores y nuevas funciones

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
